8cf76ab30998afafeff626a2772a9179b23d2156
zubair <zubair@github>
2017-12-25 09:58:46 +1000

    Adding custom events for Chat App

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
 var socket = io();
   socket.on('connect', () =>{
     console.log('connected to server');
-    socket.emit('createMsg',{
-      from:'zubi',
-      text:'Asalaamualeykum'
-    });
+
     socket.on('newMessage', function(msg){
       console.log('from server', msg);
-    })
+    });
+
+    socket.on('newUser', function(msg){
+      console.log(msg);
+    });
+
+    socket.on('newJoin', function(msg){
+      console.log(msg);
+    });
   });
   socket.on('disconnect', function(){
     console.log('disconnected from server');
--- server/server.js
+++ server/server.js
@@ -16,18 +16,28 @@ var io = socketIO(server);
 io.on('connection', (socket) =>{
   console.log('New User Connected');
 
+  socket.emit('newUser', {
+    from: 'Admin',
+    text: 'Welcome to Chat App',
+    createAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newJoin', {
+    from: 'Admin',
+    text:'New user Joined',
+    createAt: new Date().getTime()
+  });
+
   socket.on('disconnect', () =>{
     console.log('disconnected from server');
   });
 
-  socket.emit('newMessage', {
-    from:'Ammu',
-    text:'Asalaamualeykum from server',
-    createAt: 123
-  });
-
   socket.on('createMsg', function(msg){
     console.log('Create Message', msg);
+    io.emit('newMessage', {
+      from: msg.from,
+      text: msg.text
+    });
   });
 });
 
