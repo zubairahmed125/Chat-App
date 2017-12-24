b58fd173ced281b3906cfd810aaa45015d3dbb3a
zubair <zubair@github>
2017-12-24 12:20:35 +1000

    Socket IO update to App

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
-
 const app = express();
+
+const server = http.createServer(app);
+
+var io = socketIO(server);
+
+
+io.on('connection', (socket) =>{
+  console.log('New User Connected');
+socket.on('disconnect', () =>{
+    console.log('disconnected from server');
+  });
+});
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () =>{
+server.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
